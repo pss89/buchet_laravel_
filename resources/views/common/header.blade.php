8b7c1f0804a9eae2775cbb6bdc974ffc3393af4b
klinelex_sspark <sspark@klinelex.kr>
2024-03-22 13:31:05 +0900

    title 정보 환경변수 처리

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Buchet Laravel - Community</title>
+    <title>
+        {{ env('APP_TITLE') }}
+    </title>
     <!-- <link rel="stylesheet" href="styles.css">
     <script src="script.js"></script> -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
